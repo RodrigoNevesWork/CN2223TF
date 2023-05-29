756a00dde5b1835bd7e317429a8151b081b413a5
Gonçalo Moncarcho <goncalo.moncarcho@outlook.com>
2023-05-29 20:50:13 +0100

    task/publishMessageToTopic
    Primeira instância do código do publishMessageToTopic

--- TFServer/src/main/java/TFServer.java
+++ TFServer/src/main/java/TFServer.java
@@ -52,4 +52,49 @@ public class TFServer extends CNcontractGrpc.CNcontractImplBase{
         // Implement the logic for getAboveCertainty method
         // ...
     }
+
+    @Override
+    public void publishMessageToTopic(PublishMessageRequest request, StreamObserver<google.protobuf.Empty> responseObserver) {
+        // Extract the values from the request
+        Identifier identifier = request.getIdentifier();
+        String bucketName = request.getBucketName();
+        String blobName = request.getBlobName();
+
+        // Create a Pub/Sub message with the identifier, bucket name, and blob name
+        String messagePayload = "Identifier: " + identifier.getIdentifier() +
+                ", Bucket Name: " + bucketName +
+                ", Blob Name: " + blobName;
+
+        // Create a Pub/Sub ByteString to set and build the data
+        ByteString data = ByteString.copyFromUtf8(messagePayload);
+        PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
+                .setData(data)
+                .build();
+
+        // TODO Não sei como ir buscar isto
+        String projectId = "sharefotos";
+        String topicId = "tfPubSubTopic-sub";
+
+        // Create a publisher with the topic name
+        TopicName topicName = TopicName.of(projectId, topicId);
+        Publisher publisher = null;
+
+        try {
+            publisher = Publisher.newBuilder(topicName).build();
+
+            // Publish the message to the topic
+            ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
+            String messageId = messageIdFuture.get();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (publisher != null) {
+                publisher.shutdown();
+            }
+        }
+
+        // Send an empty response back to the client
+        responseObserver.onNext(google.protobuf.Empty.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
 }
